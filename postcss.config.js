b63210638b341b5435d489873bb00f0587acac3a
Eduardo Fernández <fernandez14@outlook.com>
2019-05-22 15:35:43 -0500

    chore: layout changes && purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,19 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './*.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
